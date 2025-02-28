3113976f6706624956aa3f0a775e4c454e58bbfa
sumanan24 <sumanan24@gmail.com>
2025-02-28 14:22:07 +0530

    edit

--- includes/sidenav.php
+++ includes/sidenav.php
@@ -76,6 +76,18 @@ if (!isset($_SESSION['email'])) {
                     </nav>
                 </div>
 
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#event" aria-expanded="false" aria-controls="collapseLayouts">
+                    <div class="sb-nav-link-icon"><i class="fas fa-music"></i></div>
+                    Users
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="event" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link" href="../users/new.php"><i class="fa fa-bars"></i> &nbsp;&nbsp;New</a>
+                        <a class="nav-link" href="../users/manage.php"><i class="fa fa-server"></i> &nbsp;&nbsp;Manage</a>
+                    </nav>
+                </div>
+
 
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#case" aria-expanded="false" aria-controls="collapseLayouts">
                     <div class="sb-nav-link-icon"><i class="fas fa-triangle-exclamation"></i></div>
